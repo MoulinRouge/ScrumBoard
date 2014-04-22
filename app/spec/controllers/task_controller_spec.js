c0543d62668d4114b6eedd9459b40b1b8da36aab
Priscilla Schneider <priscilla.schneider@gmx.ch>
2014-04-22 20:02:42 +0200

    Specs hinzugefügt

--- app/spec/controllers/task_controller_spec.js
+++ app/spec/controllers/task_controller_spec.js
@@ -1,30 +1,23 @@
 require('coffee-script/register');
 require('jasmine-expect');
+http=require('http');
 
-
-var controller = require('../../controllers/api/tasks');
-
-var app = {};
-app.models = {};
-app.models.tasks = require('../../models/task');
-
-var request = {};
-var response = {
-  viewName: "",
-  data: {},
-  render: function (view, viewData) {
-    this.viewName = view;
-    this.data = viewData;
-  }
-};
-
-describe("Task Controller", function () {
+describe("Servr Responce", function () {
   describe("Index", function () {
     it("renders the index view", function () {
-      controller.index(app, request, response);
-      console.log(response.viewName);
-      expect(response.viewName).toEqual("tasks");
+      http.get('http://localhost:11111', function (response){
+        expect(response.statusCode).toBe(200);
+      });
     });
 
   });
+
+  describe("Tasks", function () {
+    it("renders the tasks view", function () {
+      http.get('http://localhost:11111/#/tasks', function (response){
+        expect(response.statusCode).toBe(200);
+      });
+    });
+  });
+  
 });
