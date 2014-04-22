6d1cb421b9b339d0002fc626f186ee0ed54ffb50
Priscilla Schneider <priscilla.schneider@gmx.ch>
2014-04-22 20:06:20 +0200

    Kommentare zu Tests

--- app/spec/controllers/task_controller_spec.js
+++ app/spec/controllers/task_controller_spec.js
@@ -2,7 +2,13 @@ require('coffee-script/register');
 require('jasmine-expect');
 http=require('http');
 
+/*
+Prüft die Serverantworten
+*/
 describe("Servr Responce", function () {
+  /*
+  Ruft die Index-View auf, und prüft den HTTP-Statuscode (200)
+  */
   describe("Index", function () {
     it("renders the index view", function () {
       http.get('http://localhost:11111', function (response){
@@ -12,6 +18,9 @@ describe("Servr Responce", function () {
 
   });
 
+  /*
+  Ruft die Tasks-View auf, und prüft den HTTP-Statuscode (200)
+  */
   describe("Tasks", function () {
     it("renders the tasks view", function () {
       http.get('http://localhost:11111/#/tasks', function (response){
@@ -19,5 +28,5 @@ describe("Servr Responce", function () {
       });
     });
   });
-  
+
 });
